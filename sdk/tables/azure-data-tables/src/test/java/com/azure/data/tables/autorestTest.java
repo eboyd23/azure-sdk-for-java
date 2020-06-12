f8c15eedb88c129faba2ef1040a67dbbaab2d03a
Eleanor Boyd <t-elboy@microsoft.com>
2020-06-12 10:44:29 -0400

    test for tables SDK

--- sdk/tables/azure-data-tables/src/test/java/com/azure/data/tables/autorestTest.java
+++ sdk/tables/azure-data-tables/src/test/java/com/azure/data/tables/autorestTest.java
@@ -1,73 +1,164 @@
 package com.azure.data.tables;
 
+import com.azure.core.http.*;
+import com.azure.core.http.policy.*;
+
+import com.azure.core.util.Context;
+import com.azure.core.util.logging.ClientLogger;
 import com.azure.data.tables.implementation.AzureTableImpl;
 import com.azure.data.tables.implementation.AzureTableImplBuilder;
-import com.azure.data.tables.implementation.TablesImpl;
-import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import com.azure.data.tables.implementation.TablesImpl;
+import com.azure.data.tables.implementation.models.QueryOptions;
+import com.azure.data.tables.implementation.models.ResponseFormat;
+import com.azure.data.tables.implementation.models.TableProperties;
+import com.azure.storage.common.StorageSharedKeyCredential;
+import com.azure.storage.common.implementation.connectionstring.StorageAuthenticationSettings;
+import com.azure.storage.common.implementation.connectionstring.StorageConnectionString;
+
+import com.azure.storage.common.policy.StorageSharedKeyCredentialPolicy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class autorestTest {
 
-    private AzureTableImpl createClient() {
-        final String connectionString = System.getenv("azure_tables_connection_string");
-        final ServiceBusSharedKeyCredential credential = new ServiceBusSharedKeyCredential(
-            properties.getSharedAccessKeyName(), properties.getSharedAccessKey());
-        final List<HttpPipelinePolicy> policies = new ArrayList<>();
-        policies.add(new UserAgentPolicy());
-        policies.add(new ServiceBusTokenCredentialHttpPolicy(credential));
-        policies.add(new HttpLoggingPolicy(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS)));
 
-        final HttpClient httpClientToUse;
-        if (interceptorManager.isPlaybackMode()) {
-            httpClientToUse = interceptorManager.getPlaybackClient();
-        } else {
-            httpClientToUse = httpClient;
-            policies.add(interceptorManager.getRecordPolicy());
-            policies.add(new RetryPolicy());
-        }
+    @Test
+    void createAndUpdateTableFixed() {
+        final String connectionString = System.getenv("azure_tables_connection_string");
+        final List<HttpPipelinePolicy> policies = new ArrayList<>();
+
+        StorageConnectionString storageConnectionString
+            = StorageConnectionString.create(connectionString, new ClientLogger("tables"));
+
+        StorageAuthenticationSettings authSettings = storageConnectionString.getStorageAuthSettings();
+        StorageSharedKeyCredential sharedKeyCredential = new StorageSharedKeyCredential(authSettings.getAccount().getName(),
+            authSettings.getAccount().getAccessKey());
+
+        //storagesharedkey object and the storage auth object
+        policies.add(new AddDatePolicy());
+        policies.add(new StorageSharedKeyCredentialPolicy(sharedKeyCredential));
+        //HttpLoggingPolicy()
 
         final HttpPipeline pipeline = new HttpPipelineBuilder()
-            .httpClient(httpClientToUse)
+            .httpClient(null)
             .policies(policies.toArray(new HttpPipelinePolicy[0]))
             .build();
 
-        return new ServiceBusManagementClientImplBuilder()
-            .serializer(serializer)
-            .endpoint(properties.getEndpoint().getHost())
-            .apiVersion("2017-04")
+        AzureTableImplBuilder azureTableImplBuilder = new AzureTableImplBuilder();
+        AzureTableImpl azureTable = azureTableImplBuilder
             .pipeline(pipeline)
+            .url("https://telboytrial.table.core.windows.net/")
             .buildClient();
+
+        try{
+            TablesImpl tables = azureTable.getTables();
+
+            StepVerifier.create(tables.deleteWithResponseAsync("ebTable","ID23",Context.NONE))
+                .assertNext(response -> {
+                    System.out.println(response);
+                    Assertions.assertEquals(200, response.getStatusCode());
+                })
+                .expectComplete()
+                .verify();
+        } catch (Exception e){
+            System.out.print(e);
+        }
+
     }
-
-
-
-
-
 
     @Test
-    void createTableTest() {
-        AzureTableImplBuilder atib = new AzureTableImplBuilder()
-            .url()
-            .pipeline()
+    void TablesAuth() {
+        final String connectionString = System.getenv("azure_tables_connection_string");
+        final List<HttpPipelinePolicy> policies = new ArrayList<>();
+
+        StorageConnectionString storageConnectionString
+            = StorageConnectionString.create(connectionString, new ClientLogger("tables"));
+        System.out.println(storageConnectionString);
+
+        StorageAuthenticationSettings authSettings = storageConnectionString.getStorageAuthSettings();
+
+        TablesSharedKeyCredential sharedKeyCredential = new TablesSharedKeyCredential(authSettings.getAccount().getName(),
+            authSettings.getAccount().getAccessKey());
+
+        //storagesharedkey object and the storage auth object
+        policies.add(new AddDatePolicy());
+        policies.add(new TablesSharedKeyCredentialPolicy(sharedKeyCredential));
+        //HttpLoggingPolicy()
+
+        final HttpPipeline pipeline = new HttpPipelineBuilder()
+            .httpClient(null)
+            .policies(policies.toArray(new HttpPipelinePolicy[0]))
+            .build();
+
+        AzureTableImplBuilder azureTableImplBuilder = new AzureTableImplBuilder();
+        AzureTableImpl azureTable = azureTableImplBuilder
+            .pipeline(pipeline)
+            .url("https://telboytrial.table.core.windows.net/")
+            .buildClient();
+
+        try{
+            TablesImpl tables = azureTable.getTables();
+
+            StepVerifier.create(tables.createWithResponseAsync(new TableProperties().setTableName("ebTable"),
+                "ID23",
+                ResponseFormat.RETURN_CONTENT,
+                null,null))
+                .assertNext(response -> {
+                    System.out.println(response);
+                    Assertions.assertEquals(200, response.getStatusCode());
+                })
+                .expectComplete()
+                .verify();
+        } catch (Exception e){
+            System.out.print(e);
+        }
+
+//        try{
+//            TablesImpl tables = azureTable.getTables();
+//
+//            StepVerifier.create(tables.deleteWithResponseAsync("ebTable","ID23",Context.NONE))
+//                .assertNext(response -> {
+//                    System.out.println(response);
+//                    Assertions.assertEquals(200, response.getStatusCode());
+//                })
+//                .expectComplete()
+//                .verify();
+//        } catch (Exception e){
+//            System.out.print(e);
+//        }
 
     }
 
 
-    @Test
-    void createAndUpdateTable() throws InterruptedException {
-    }
 
-    /**
-     * We've fixed this by caching the result of the `createTable` operation if it is successful. So we don't try to
-     * create the table again.
-     *
-     * See {@link #createAndUpdateTable()}
-     */
-    @Test
-    void createAndUpdateTableFixed() throws InterruptedException {
 
-    }
+
+
+
+
+
+//    @Test
+//    void createTableTest() {
+//        StringToSign = VERB + "\n" +
+//            Content-MD5 + "\n" +
+//            Content-Type + "\n" +
+//            Date + "\n" +
+//            CanonicalizedResource;
+//
+//    }
+//
+//
+//    @Test
+//    void createAndUpdateTable() throws InterruptedException {
+//        TablesImpl ti = new TablesImpl( new AzureTableImpl());
+//        Mono<TablesCreateResponse> c = ti.createWithResponseAsync( new TableProperties(), "requestId", RETURN_CONTENT,
+//            null , new Context("key", "value"));
+//    }
+
 }

fece694303e0468d812eb3cc693d0b2a381e28d9
Eleanor Boyd <t-elboy@microsoft.com>
2020-07-06 12:38:48 -0400

    try15edits based on comments

--- sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/TableServiceAsyncClient.java
+++ sdk/tables/azure-data-tables/src/main/java/com/azure/data/tables/TableServiceAsyncClient.java
@@ -48,12 +48,12 @@ public class TableServiceAsyncClient {
     }
 
     /**
-     * retrieves the table client for the provided table or creates one if it doesn't exist
+     * retrieves the async table client for the provided table or creates one if it doesn't exist
      *
      * @param name the name of the table
-     * @return associated TableClient
+     * @return associated TableAsyncClient
      */
-    public Mono<TableClient> getTableClient(String name) {
+    public TableAsyncClient getTableAsyncClient(String name) {
         return null;
     }
 
